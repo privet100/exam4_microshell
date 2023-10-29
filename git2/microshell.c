c0957472d3bd31508905cb99f185e2d6f366ba29
akostrik <internet124816@gmail.com>
2023-10-29 12:25:20 +0100

    .

--- git2/microshell.c
+++ git2/microshell.c
@@ -23,53 +23,58 @@ void	write_fd2(char *s1, char *s2)
 
 int	main(int argc, char *argv[], char *env[])
 {
-	int	i;
+	int	i = 0;
+	int j = 0;
+	int fd_tmp = dup(STDIN);
 	int pip[2];
-	int fd_tmp;
-	(void)argc;	// -Wall -Wextra -Werror
+	(void)argc;
 
-	i = 0;
-	CHILDIN = dup(STDIN);
-	dprintf(2, "\nPPP 0   %d -> ... -> %d,               %d\n", STDIN, STDOUT, CHILDIN);
+	// dprintf(2, "\nGEN-%d %2d -> ... -> %2d,                 %2d\n", j, STDIN, STDOUT, CHILDIN);
 	while (argv[i] && argv[i + 1]) //check the end
 	{
-
-		argv = &argv[i + 1]; //new argv start after ; or |
+		argv = &argv[i + 1]; //new argv starts after ; or |
 		i = -1;
 		while (argv[++i] && strcmp(argv[i], ";") && strcmp(argv[i], "|")) ;
 		if (strcmp(argv[0], "cd") == 0 && i == 2)
 		{
 			if (chdir(argv[1]) != 0)
-				write_fd2("error: cd: cannot change directory to ", argv[1]); // do not exit
+				write_fd2("error: cd: cannot change directory to ", argv[1]);
 		}
 		else if (strcmp(argv[0], "cd") == 0 && i != 2)
-			write_fd2("error: cd: bad arguments", NULL);                      // do not exit
+			write_fd2("error: cd: bad arguments", NULL);
 		else if(i > 0 && (argv[i] == NULL || strcmp(argv[i], "|") == 0 || strcmp(argv[i], ";") == 0) && pipe(pip) == 0)
 		{
-			dprintf(2, "\nPPP (%d) %d -> ... -> %d, %d -> ... -> %d %d\n", i, STDIN, STDOUT, CHILDIN, CHILDOUT, NXT_CHILDIN);
+			// dprintf(2, "\nGEN-%d                  %2d -> ... -> %2d %2d\n", j, CHILDIN, CHILDOUT, NXT_CHILDIN);
 			if (fork() != 0)
 			{
-				CHILDIN = NXT_CHILDIN; // close(fd[1]); close(tmp_f d);
-				dprintf(2, "PAR (%d) %d -> ... -> %d, %d -> ... -> %d %d\n", i, STDIN, STDOUT, CHILDIN, CHILDOUT, NXT_CHILDIN);
-				// while(waitpid(-1, NULL, WUNTRACED) != -1) ; // close(tmp_f d), waits child complete / enter a stopped state, WUNTRACED = child has stopped (but not traced via ptrace
-				wait(NULL);
+				close(CHILDIN);
+				CHILDIN = NXT_CHILDIN;
+				close(CHILDOUT);
+				// dprintf(2, "PAR-%d %2d -> ... -> %2d,                 %2d\n\n", j, STDIN, STDOUT,  NXT_CHILDIN);
+				// while(waitpid(-1, NULL, WUNTRACED) != -1) ; // close(tmp_f d), waits child complete / stopped, WUNTRACED = stopped but not traced via ptrace
+				waitpid(-1, NULL, WUNTRACED); // close(tmp_f d), waits child complete / stopped, WUNTRACED = stopped but not traced via ptrace
 			}
 			else
 			{
 				if (argv[i] == NULL || strcmp(argv[i], ";") == 0)
+				{
+					close(CHILDOUT);
 					CHILDOUT  = dup(STDOUT);
-				if (argv[i] == NULL || strcmp(argv[i], ";") == 0)
+					close(NXT_CHILDIN);
 					NXT_CHILDIN = dup(STDIN);
-				dup2(CHILDIN, STDIN); // close(tmp_f d);
-				dup2(CHILDOUT, STDOUT); // close(fd[0]); close(fd[1]);
-				argv[i] = NULL; // overwrite ; | NULL with NULL -> input for execve, no impact in the parent process
-				dprintf(2, "CHL (%d) %d -> ... -> %d, %d -> ... -> %d %d\n", i, CHILDIN, CHILDOUT, CHILDIN, CHILDOUT, NXT_CHILDIN);
+				}
+				dup2(CHILDIN, STDIN);
+				close(CHILDIN);
+				dup2(CHILDOUT, STDOUT); // close(fd[0]);
+				close(CHILDOUT);
+				argv[i] = NULL; // overwrite ; | NULL with NULL -> no impact in the parent
+				// dprintf(2, "CHL-%d %2d -> ... -> %2d, %2d -> ... -> %2d %2d\n", j, CHILDIN, CHILDOUT, CHILDIN, CHILDOUT, NXT_CHILDIN);
 				execve(argv[0], argv, env);
-				write_fd2("error: cannot execute ", argv[0]);                // do not exit
+				write_fd2("error: cannot execute ", argv[0]);
 			}
 		}
-		//sleep(1000);
+		j++;
 	}
-	// close(CHILDIN);
+	close(CHILDIN);
 	//while (1); // if test
 }
