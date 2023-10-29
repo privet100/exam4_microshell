1dd921006cd4ccfe1c399e1d642fc0cb17f8a09c
akostrik <internet124816@gmail.com>
2023-10-29 13:13:04 +0100

    .

--- microshell.c
+++ microshell.c
@@ -1,8 +1,14 @@
+	// dprintf(2, "\nGEN-%d %2d -> ... -> %2d,                 %2d\n", j, STDIN, STDOUT, CHILDIN);
+			// dprintf(2, "\nGEN-%d                  %2d -> ... -> %2d %2d\n", j, CHILDIN, CHILDOUT, NXT_CHILDIN);
+				// dprintf(2, "PAR-%d %2d -> ... -> %2d,                 %2d\n\n", j, STDIN, STDOUT,  NXT_CHILDIN);
+				// dprintf(2, "CHL-%d %2d -> ... -> %2d, %2d -> ... -> %2d %2d\n", j, CHILDIN, CHILDOUT, CHILDIN, CHILDOUT, NXT_CHILDIN);
+// #include <stdlib.h> // tmp
+// #include <stdio.h> // tmp
+	//while (1); // if test
+
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
-#include <stdlib.h> // tmp
-#include <stdio.h> // tmp
 #define CHILDIN     fd_tmp
 #define CHILDOUT    pip[1]
 #define NXT_CHILDIN pip[0]
@@ -26,7 +32,6 @@ int	main(int argc, char *argv[], char *env[])
 	int pip[2];
 	(void)argc;
 
-	// dprintf(2, "\nGEN-%d %2d -> ... -> %2d,                 %2d\n", j, STDIN, STDOUT, CHILDIN);
 	while (argv[i] && argv[i + 1]) //check the end
 	{
 		argv = &argv[i + 1]; //new argv starts after ; or |
@@ -41,14 +46,11 @@ int	main(int argc, char *argv[], char *env[])
 			write_fd2("error: cd: bad arguments", NULL);
 		else if(i > 0 && (argv[i] == NULL || strcmp(argv[i], "|") == 0 || strcmp(argv[i], ";") == 0) && pipe(pip) == 0)
 		{
-			// dprintf(2, "\nGEN-%d                  %2d -> ... -> %2d %2d\n", j, CHILDIN, CHILDOUT, NXT_CHILDIN);
 			if (fork() != 0)
 			{
 				close(CHILDIN);
 				CHILDIN = NXT_CHILDIN;
 				close(CHILDOUT);
-				// dprintf(2, "PAR-%d %2d -> ... -> %2d,                 %2d\n\n", j, STDIN, STDOUT,  NXT_CHILDIN);
-				// while(waitpid(-1, NULL, WUNTRACED) != -1) ; // close(tmp_f d), waits child complete / stopped, WUNTRACED = stopped but not traced via ptrace
 				waitpid(-1, NULL, WUNTRACED); // close(tmp_f d), waits child complete / stopped, WUNTRACED = stopped but not traced via ptrace
 			}
 			else
@@ -65,7 +67,6 @@ int	main(int argc, char *argv[], char *env[])
 				dup2(CHILDOUT, STDOUT); // close(fd[0]);
 				close(CHILDOUT);
 				argv[i] = NULL; // overwrite ; | NULL with NULL -> no impact in the parent
-				// dprintf(2, "CHL-%d %2d -> ... -> %2d, %2d -> ... -> %2d %2d\n", j, CHILDIN, CHILDOUT, CHILDIN, CHILDOUT, NXT_CHILDIN);
 				execve(argv[0], argv, env);
 				write_fd2("error: cannot execute ", argv[0]);
 			}
@@ -73,5 +74,4 @@ int	main(int argc, char *argv[], char *env[])
 		j++;
 	}
 	close(CHILDIN);
-	//while (1); // if test
 }
