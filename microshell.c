a2c56a09eacc165af0f81c2c53bda3b0b9c45c60
ann <internet124816@gmail.com>
2023-11-02 19:34:47 +0100

    final

--- microshell.c
+++ microshell.c
@@ -3,10 +3,8 @@
 #include <string.h>
 #define CHILDOUT    pip[1]
 #define NXT_CHILDIN pip[0]
-#define STDIN       STDIN_FILENO
-#define STDOUT      STDOUT_FILENO
 
-void	write_fd2(char *s1, char *s2)
+void	write_fd2(char *s1, char *s2)	
 {
 	while (*s1)
 		write(2, s1++, 1);
@@ -15,42 +13,43 @@ void	write_fd2(char *s1, char *s2)
 	write(2, "\n", 1);
 }
 
-int	main(int argc, char *argv[], char *env[])
+int	main(int ac, char **av, char **env)
 {
 	int	i = 0;
-	int CHILDIN = dup(STDIN);
+	int CHILDIN = dup(STDIN_FILENO);
 	int pip[2];
-	(void)argc;
+	(void)ac;
 
-	while (argv[i] && argv[i + 1]) //check the end
+	while (av[i] && av[i + 1]) //check the end
 	{
-		for (argv = &argv[i + 1], i = 0; argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"); i++) ; //new argv starts after ; or |
-		if (strcmp(argv[0], "cd") == 0 && i != 2)
+		for (av = &av[i + 1], i = 0; av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"); i++) ; //new av starts after ; or |
+		if (i > 0  && strcmp(av[0], "cd") == 0 && (i != 2 || (i == 2 && av[1][0] == '-')))
 			write_fd2("error: cd: bad arguments", NULL);
-		if (strcmp(argv[0], "cd") == 0 && i == 2 && chdir(argv[1]) != 0)
-			write_fd2("error: cd: cannot change directory to ", argv[1]);
-		if (strcmp(argv[0], "cd") != 0 && i > 0 && (!argv[i] || !strcmp(argv[i], "|") || !strcmp(argv[i], ";")) && !pipe(pip))
+		if (i == 2 && strcmp(av[0], "cd") == 0 && chdir(av[1]) != 0)
+			write_fd2("error: cd: cannot change directory to ", av[1]);
+		if (i > 0  && strcmp(av[0], "cd") != 0 && (!av[i] || !strcmp(av[i], "|") || !strcmp(av[i], ";")) && !pipe(pip))
 		{
 			if (fork() != 0)
 			{
 				close(CHILDIN);
 				CHILDIN = NXT_CHILDIN;
 				close(CHILDOUT);
-				waitpid(-1, NULL, WUNTRACED); // waits child complete / stopped, WUNTRACED = stopped but not traced via ptrace
+				waitpid(-1, NULL, WUNTRACED); // child complete / stopped, WUNTRACED = stopped but not traced via ptrace
 			}
 			else
 			{
-				dup2 (CHILDIN, STDIN);
+				dup2 (CHILDIN, STDIN_FILENO);
 				close(CHILDIN);
-				if (argv[i] != NULL && strcmp(argv[i], "|") == 0)
-					dup2 (CHILDOUT, STDOUT);
+				if (av[i] != NULL && strcmp(av[i], "|") == 0)
+					dup2 (CHILDOUT, STDOUT_FILENO);
 				close(CHILDOUT);
 				close(NXT_CHILDIN);
-				argv[i] = NULL; // overwrite ; | NULL with NULL, no impact in the parent
-				execve(argv[0], argv, env);
-				write_fd2("error: cannot execute ", argv[0]);
+				av[i] = NULL; // overwrite ; and |  with NULL, no impact in the parent
+				execve(av[0], av, env);
+				write_fd2("error: cannot execute ", av[0]);
 			}
 		}
 	}
 	close(CHILDIN);
+	return (1);
 }
